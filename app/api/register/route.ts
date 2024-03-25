ab0d4037f29a530d195051ea62acc6b43046b85a
boluwatifeajayi <bolu4good@gmail.com>
2024-03-25 16:52:49 +0100

    hmmm

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -3,22 +3,24 @@ import bcrypt from "bcrypt";
 
 import prisma from "@/app/libs/prismadb";
 
-export async function POST(request: Request) {
+export async function POST(
+  request: Request, 
+) {
   const body = await request.json();
-  const { email, name, password, role = "ADMIN" } = body; // Set default role
+  const { 
+    email,
+    name,
+    password,
+   } = body;
 
-  if (!email || !name || !password) {
-    return NextResponse.json({ message: "Missing required fields" }, { status: 400 });
-  }
+   const hashedPassword = await bcrypt.hash(password, 12);
 
-  const hashedPassword = await bcrypt.hash(password, 12);
-
-  const user = await prisma.user.create({
+   const user = await prisma.user.create({
     data: {
       email,
       name,
       hashedPassword,
-    },
+    }
   });
 
   return NextResponse.json(user);
